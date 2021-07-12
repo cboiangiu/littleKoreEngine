36fab7dc6ec87b6caa0cb2f7ada55004d53e3808
Catalin Boiangiu <catalin.boiangiu@lansoft.ro>
2021-07-12 22:46:18 +0300

    tutorial00: test math operations

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,8 +1,15 @@
 #include <Kore/System.h>
+#include <Kore/Math/Matrix.h>
+#include <Kore/Math/Vector.h>
 
 int kickstart(int argc, char** argv) {
 	Kore::System::init("Kore window", 800, 600);
 
+    Kore::mat4 matrix;
+    Kore::vec4 vec;
+    
+    auto test = matrix * vec;
+    
 	Kore::System::start();
 
 	return 0;
